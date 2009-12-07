be719b0063f3503702435f82ef14572f8ac10c3f
Rob Cooper (robcthegeek) <robcthegeek.public@gmail.com>
2009-12-07 19:48:37 +0000

    Created Project Shell

---
+++ ASPUtils/ASPUtils.Tests.Unit/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace robcthegeek.ASPUtils.Tests.Unit
+{
+    public class TestClass
+    {
+        [Fact]
+        public void SomeFailingTest()
+        {
+            Assert.False(true);
+        }
+    }
+}
