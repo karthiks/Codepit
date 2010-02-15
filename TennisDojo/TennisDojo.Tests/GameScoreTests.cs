3a7a562248878e3e2a09331ad48be9b5559e2aa5
Rob Cooper <robcthegeek.public@gmail.com>
2010-02-15 10:01:28 +0000

    Added 2nd Passing Test and Plan for Up To Adv.

--- TennisDojo/TennisDojo.Tests/GameScoreTests.cs
+++ TennisDojo/TennisDojo.Tests/GameScoreTests.cs
@@ -5,12 +5,46 @@ namespace TennisDojo.Tests
 {
     public class GameScoreTests
     {
+        static GameScore CreateGameScore()
+        {
+            return new GameScore();
+        }
+
         [Fact]
         public void Ctor_NoPointsScored_BothPlayersHaveZeroPoints()
         {
-            var score = new GameScore();
+            var score = CreateGameScore();
             Assert.Equal(0, score.Player1Score);
             Assert.Equal(0, score.Player2Score);
+        }
+
+        [Fact]
+        public void Player1Score_Player1ScoresOnce_Returns1()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer1(1);
+            Assert.Equal(1, score.Player1Score);
+        }
+
+        [Fact]
+        public void Player2Score_Player1ScoresOnce_Returns0()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer1(1);
+            Assert.Equal(0, score.Player2Score);
+        }
+    }
+
+
+    public static class GameScoreExtensions
+    {
+        public static GameScore ScorePointsForPlayer1(this GameScore score, int points)
+        {
+            for (int i = 0; i < points; i++)
+            {
+                score.Player1Scored();
+            }
+            return score;
         }
     }
 }
--- TennisDojo/TennisDojo/GameScore.cs
+++ TennisDojo/TennisDojo/GameScore.cs
@@ -1,8 +1,15 @@
+using System;
+
 namespace TennisDojo.Core
 {
     public class GameScore
     {
-        public int Player1Score;
+        public int Player1Score { get; private set; }
         public int Player2Score;
+
+        public void Player1Scored()
+        {
+            Player1Score++;
+        }
     }
 }
