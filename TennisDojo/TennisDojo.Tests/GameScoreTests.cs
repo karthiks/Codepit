9eed54bab6a0bb4cfbfcaed179c72e29ee32bb07
Rob Cooper <robcthegeek.public@gmail.com>
2010-02-15 10:07:58 +0000

    Passed Basic Scoring Tests

--- TennisDojo/TennisDojo.Tests/GameScoreTests.cs
+++ TennisDojo/TennisDojo.Tests/GameScoreTests.cs
@@ -33,6 +33,58 @@ namespace TennisDojo.Tests
                 .ScorePointsForPlayer1(1);
             Assert.Equal(0, score.Player2Score);
         }
+
+        [Fact]
+        public void Player2Score_Player2ScoresOnce_Returns1()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer2(1);
+            Assert.Equal(1, score.Player2Score);
+        }
+
+        [Fact]
+        public void Player1Score_Player2ScoresOnce_Returns0()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer2(1);
+            Assert.Equal(0, score.Player1Score);
+        }
+
+        [Fact]
+        public void Player1Score_BothPlayersScoreOnce_Returns1()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer1(1)
+                .ScorePointsForPlayer2(1);
+            Assert.Equal(1, score.Player1Score);
+        }
+
+        [Fact]
+        public void Player2Score_BothPlayersScoreOnce_Returns1()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer1(1)
+                .ScorePointsForPlayer2(1);
+            Assert.Equal(1, score.Player2Score);
+        }
+
+        [Fact]
+        public void Player1Score_BothPlayersScoreTwice_Returns2()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer1(2)
+                .ScorePointsForPlayer2(2);
+            Assert.Equal(2, score.Player1Score);
+        }
+
+        [Fact]
+        public void Player2Score_BothPlayersScoreTwice_Returns2()
+        {
+            var score = CreateGameScore()
+                .ScorePointsForPlayer1(2)
+                .ScorePointsForPlayer2(2);
+            Assert.Equal(2, score.Player2Score);
+        }
     }
 
 
@@ -46,5 +98,15 @@ namespace TennisDojo.Tests
             }
             return score;
         }
+
+        public static GameScore ScorePointsForPlayer2(this GameScore score, int points)
+        {
+            for (int i = 0; i < points; i++)
+            {
+                score.Player2Scored();
+            }
+            return score;
+        }
+
     }
 }
--- TennisDojo/TennisDojo/GameScore.cs
+++ TennisDojo/TennisDojo/GameScore.cs
@@ -5,11 +5,16 @@ namespace TennisDojo.Core
     public class GameScore
     {
         public int Player1Score { get; private set; }
-        public int Player2Score;
+        public int Player2Score { get; private set; }
 
         public void Player1Scored()
         {
             Player1Score++;
+        }
+
+        public void Player2Scored()
+        {
+            Player2Score++;
         }
     }
 }
