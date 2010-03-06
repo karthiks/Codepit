0f136fcd7a6d8b4b373ea972efcc38fe57eb9c68
Rob Cooper (robcthegeek) <robcthegeek.public@gmail.com>
2010-03-06 09:14:27 +0000

    Added DeniedFilter

---
+++ MvcFiltersAndModelBinders/MvcFiltersAndModelBinders/Controllers/FiltersController.cs
@@ -0,0 +1,23 @@
+using System.Web.Mvc;
+using MvcFiltersAndModelBinders.Models.Filters;
+
+namespace MvcFiltersAndModelBinders.Controllers
+{
+    public class FiltersController : Controller
+    {
+        //
+        // GET: /Filters/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [DeniedFilter]
+        public ActionResult Denied()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcFiltersAndModelBinders/MvcFiltersAndModelBinders/Models/Filters/DeniedFilter.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace MvcFiltersAndModelBinders.Models.Filters
+{
+    // Inherit from FilterAttribute, NOT Attribute
+    public class DeniedFilter : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            // HttpUnauthorizedResult Returns a HTTP 401 (Unauthorized)
+            filterContext.Result = 
+                new HttpUnauthorizedResult();
+        }
+    }
+}
